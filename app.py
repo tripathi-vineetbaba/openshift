53ee0f431e91fbce78324910f53babe62ef88339
Vineet <tripathi.vineetbaba@gmail.com>
2021-01-17 13:15:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+'''if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)'''
